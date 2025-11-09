a977571d71ddd03556265a5b652da9a527a4e969
Vicentesan <vikom.sanchez@gmail.com>
2025-11-09 19:38:59 -0300

    feat: add svgs

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,25 +1,51 @@
+import { Illustrations } from '@/components/illustrations';
+import { Logos } from '@/components/logos';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <div className="flex flex-col items-center gap-7">
-        <div className="flex flex-col items-center gap-2.5">
-          <h1 className="w-fit max-w-4xl text-center font-redaction text-7xl">
-            Track, review and share every game you play
-          </h1>
-          <p className="max-w-2xl text-center font-geist-sans font-medium text-muted-foreground text-xl">
-            Unlock a new way to track your gaming experiences, connect with friends, and find out
-            what’s trending.
-          </p>
+    <div className="flex min-h-screen flex-col items-center justify-center gap-7">
+      <div className="fixed top-15">
+        <Logos.Joysticked />
+      </div>
+
+      <div className="fixed bottom-0 flex max-w-screen flex-row items-end">
+        <Illustrations.Elephant className="-translate-y-[60%] absolute z-10 translate-x-[520%]" />
+
+        <div className="relative">
+          <Illustrations.HalfPortal className="z-0" />
+          <Illustrations.Flower className="absolute bottom-0 z-1" />
         </div>
-        <div className="flex flex-col items-center gap-7">
-          <Input placeholder="kratos@godofwar.com" className="w-80" />
-          <Button variant="default" className="w-2/3 font-bold font-geist-sans">
-            Join our waitlist
-          </Button>
+
+        <div className="relative flex items-end">
+          <Illustrations.LeftFloor className="z-0" />
+          <Illustrations.Pipe className="absolute bottom-0 z-1 translate-x-[650%]" />
+          <Illustrations.RightFloor className="z-0" />
+
+          <div className="absolute right-10 bottom-0 flex flex-col">
+            <Illustrations.FlowerThree className="-translate-x-1/3 mb-12 rotate-[-10deg]" />
+            <Illustrations.FlowerTwo />
+          </div>
         </div>
+      </div>
+
+      <div className="flex flex-col items-center gap-2.5">
+        <h1 className="w-fit max-w-4xl text-center font-redaction text-7xl">
+          Track, review and share every game you play
+        </h1>
+
+        <p className="max-w-2xl text-center font-geist-sans font-medium text-muted-foreground text-xl">
+          Unlock a new way to track your gaming experiences, connect with friends, and find out
+          what’s trending.
+        </p>
+      </div>
+
+      <div className="z-10 flex flex-col items-center gap-7">
+        <Input placeholder="kratos@godofwar.com" className="w-80 bg-neutral-800" />
+        <Button variant="default" className="w-2/3 py-1 font-bold font-geist-sans">
+          Join our waitlist
+        </Button>
       </div>
     </div>
   );
---
+++ apps/web/src/components/illustrations/index.ts
@@ -0,0 +1,19 @@
+import { Elephant } from './elephant';
+import { Flower } from './flower';
+import { FlowerThree } from './flower-three';
+import { FlowerTwo } from './flower-two';
+import { HalfPortal } from './half-portal';
+import { LeftFloor } from './left-floor';
+import { Pipe } from './pipe';
+import { RightFloor } from './right-floor';
+
+export const Illustrations = {
+  HalfPortal,
+  Flower,
+  LeftFloor,
+  Pipe,
+  RightFloor,
+  FlowerTwo,
+  FlowerThree,
+  Elephant
+};
