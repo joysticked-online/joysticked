500148c9cfdaec369cb2331012aa89fab611e320
guisandroni <gui.sandroni@gmail.com>
2025-11-14 00:39:08 -0300

    chore: update API/Web configs, dependencies and email provider setup for new auth flow

--- apps/api/src/shared/config/envs.ts
+++ apps/api/src/shared/config/envs.ts
@@ -3,7 +3,8 @@ import { z } from 'zod';
 export const envs = {
   app: loadAppEnvs(),
   db: loadDbEnvs(),
-  services: loadServicesEnvs()
+  services: loadServicesEnvs(),
+  auth: loadAuthEnvs(),
 };
 
 function loadAppEnvs() {
@@ -31,5 +32,18 @@ function loadServicesEnvs() {
     EMAIL_DOMAIN: z.string()
   });
 
+  return schema.parse(process.env);
+}
+
+
+function loadAuthEnvs() {
+  const schema = z.object({
+    BETTER_AUTH_SECRET: z.string().min(32),
+    BETTER_AUTH_URL: z.url().default('http://localhost:8080'),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
+    DISCORD_CLIENT_ID: z.string(),
+    DISCORD_CLIENT_SECRET: z.string()
+  });
   return schema.parse(process.env);
 }
--- apps/api/src/shared/database/schemas/index.ts
+++ apps/api/src/shared/database/schemas/index.ts
@@ -1,1 +1,2 @@
 export * from './waitlists';
+export * from './auth';
